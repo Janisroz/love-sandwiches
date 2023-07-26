f0689c8d0ed2f9608cdfebe8b7aec173ffff41a6
Janisroz <janisrozkalejs@gmail.com>
2023-07-26 18:57:26 +0000

    Adjust validation to check that all values provided are intergers

--- run.py
+++ run.py
@@ -12,7 +12,7 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
- 
+
 def get_sales_data():
     """
     Get sales figures input from the user
@@ -28,21 +28,20 @@ def get_sales_data():
     validate_data(sales_data)
     
  
-
 def validate_data(values):
     """
     Inside the try, converts all string values into intergers.
     Raises ValueError, if strings cannot be converted into int,
     or if there aren't exactly 6 values
     """
-
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
-            f"Exactly 6 values requred, you provied {len(values)}"
-        )
+                f"Exactly 6 values requred, you provied {len(values)}"
+            )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again\n")
 
-        
+
 get_sales_data()
